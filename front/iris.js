2ecd2bdef299cd6cb2381f4430e7f2f1a84ec5f3
sooho <soho5445@g.shingu.ac.kr>
2025-03-24 11:02:46 +0900

    -

--- front/iris.js
+++ front/iris.js
@@ -1,3 +1,23 @@
+function drawChart(probability, pid)
+{
+        var data = google.visualization.arrayToDataTable([
+          ['iris species', 'probability'],
+          ['setosa',     probability[0][0]],
+          ['versicolor',      probability[0][1]],
+          ['virginica',  probability[0][2]],
+        ]);
+
+        var options = {
+          title: '붓꽃 품종 확률',
+            is3d: true,
+            pieHole: 0.41
+        };
+
+        var chart = new google.visualization.PieChart(pid);
+
+        chart.draw(data, options);
+}
+
 function Send()
 {
     sl = document.getElementById('sl').value
@@ -24,6 +44,9 @@ function Send()
     }).done(function(response) {
         var output = document.getElementById('txtOut');
         output.innerText = 'prediction : ' + response.prediction + "일 확률: " + response.probability
+
+        google.charts.setOnLoadCallback(drawChart(response.probability, document.getElementById('piechart')))
+
     }).fail(function(response) {
         alert("fail" + JSON.stringify(response))
     }).always(function() {
